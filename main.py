89657e1ebc6f1dd85cfce3753a942e81698c2d9d
shahzaibWaseem27 <110247209+shahzaibWaseem27@users.noreply.github.com>
2023-02-07 16:01:08 +0100

    fixed bug inside else
    before, in the else block, the first and second page urls were not being read again by the user. it is fixed now

--- main.py
+++ main.py
@@ -14,5 +14,6 @@ while True:
     else:
         print("invalid input, try again\n")
         bikeCategory = input("enter merida or norco\n")
-        url = input("please enter the url of the bike category, showing 108 items per page\n")
+        first_page_URL = input("please enter the first page url of the bike category, showing 108 items per page\n")
+        second_page_URL = input("please enter the second page url of the bike category, showing 108 items per page\n")
         is_bikeCategory_Valid = bikeCategory == "merida" or bikeCategory == "norco"
